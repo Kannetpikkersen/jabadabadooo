efd2dd8a7c4502725e4e4caaaa5c1cb52f1c13ea
Wave69Zero <tostoscar@gmail.com>
2024-02-29 15:38:35 +0100

    setup boilerplate

--- src/App.java
+++ src/App.java
@@ -1,9 +1,24 @@
 import processing.core.*;
 
 public class App extends PApplet {
+
+    int c;
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         App myApp = new App();
         myApp.runSketch();
+    }
+
+    public void settings(){
+        size(600,400);
+    }
+
+    public void setup(){
+        c = color(255,0,0);
+
+    }
+
+    public void draw(){
+        background(c);
     }
 }
